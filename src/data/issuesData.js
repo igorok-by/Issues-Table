4f7fe7ea209ebe4787baeac9565b752b312b12da
Igor <igorko.by@gmail.com>
2020-11-26 12:26:17 +0300

    feat: add mock data

---
+++ src/data/issuesData.js
@@ -0,0 +1,45 @@
+export const issuesData = [
+  {
+    severity: 1,
+    userInfo: {
+      name: 'Captain America',
+      position: 'Team-lead',
+    },
+    location: 'home',
+    description: 'blah blah blah',
+    dateOfIssue: '2020-11-23',
+  },
+  {
+    severity: 1,
+    userInfo: {
+      name: 'Natasha Romanoff',
+      position: 'CEO',
+    },
+    location: 'office',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam quae ipsa pariatur cupiditate nobis fuga hic aspernatur sed blanditiis tempora. Dignissimos!',
+    dateOfIssue: '2020-11-19',
+  },
+  {
+    severity: 1,
+    userInfo: {
+      name: 'Iron Man',
+      position: 'Remote worker',
+    },
+    location: 'home',
+    description:
+      'Nam quae ipsa pariatur cupiditate nobis fuga hic aspernatur sed blanditiis tempora, obcaecati dignissimos accusantium omnis vitae quasi fugiat?',
+    dateOfIssue: '2020-11-11',
+  },
+  {
+    severity: 1,
+    userInfo: {
+      name: 'Spider-man',
+      position: 'Developer',
+    },
+    location: 'office',
+    description:
+      'Nam quae ipsa pariatur cupiditate nobis fuga hic aspernatur sed blanditiis tempora, obcaecati dignissimos accusantium omnis vitae quasi fugiat? Aliquam, alias. Dignissimos!',
+    dateOfIssue: '2020-11-26',
+  },
+]
