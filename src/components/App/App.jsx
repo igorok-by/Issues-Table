08fe19c155c698f54b71e7b6d5e1b7bb642bb9ec
Igor <igorko.by@gmail.com>
2020-11-27 07:21:24 +0300

    feat: add state of issues to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { issuesData, STATUSES, DEFAULT_COMMENT } from '../../data'
@@ -7,17 +7,20 @@ import HomePage from '../HomePage'
 import './App.scss'
 
 const App = () => {
-  const rowsData = issuesData.map((issue) => ({
-    ...issue,
-    status: STATUSES[0],
-    comment: DEFAULT_COMMENT,
-  }))
+  const [issues, setIssues] = useState(
+    issuesData.map((issue) => ({
+      ...issue,
+      status: STATUSES[0],
+      comment: DEFAULT_COMMENT,
+      isDeleted: false,
+    })),
+  )
 
   return (
     <Router>
       <Switch>
         <Route path="/">
-          <HomePage rowsData={rowsData} />
+          <HomePage rowsData={issues} />
         </Route>
         <Route path="/login">
           <LoginPage />
